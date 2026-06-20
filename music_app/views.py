dad60f72e01301b22d79db321470b700c0a7c119
Eisha-Tir-Raazia <eashasohail2233@gmail.com>
2026-06-20 16:30:25 +0500

    Add complete CRUD operations for songs (create, read, update, delete)

--- music_app/views.py
+++ music_app/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Song
+from .forms import SongForm
+
 
 def home(request):
     songs = Song.objects.all()
     return render(request, 'music_app/home.html', {'songs': songs})
+
+
 def profile(request):
     return render(request, 'music_app/profile.html')
-from django.shortcuts import redirect
-from .forms import SongForm
+
 
 def add_song(request):
     if request.method == "POST":
@@ -19,3 +22,25 @@ def add_song(request):
         form = SongForm()
 
     return render(request, 'music_app/add_song.html', {'form': form})
+
+
+def edit_song(request, song_id):
+    song = get_object_or_404(Song, id=song_id)
+    if request.method == "POST":
+        form = SongForm(request.POST, instance=song)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = SongForm(instance=song)
+
+    return render(request, 'music_app/edit_song.html', {'form': form, 'song': song})
+
+
+def delete_song(request, song_id):
+    song = get_object_or_404(Song, id=song_id)
+    if request.method == "POST":
+        song.delete()
+        return redirect('home')
+
+    return render(request, 'music_app/delete_song.html', {'song': song})
